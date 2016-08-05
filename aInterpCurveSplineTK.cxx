d0e067736b10673833eba760db5ef07066d54a6a
Polina Pasevina <rigiemp@gmail.com>
2016-08-06 00:56:19 +0300

    initSpline method added

--- aInterpCurveSplineTK.cxx
+++ aInterpCurveSplineTK.cxx
@@ -1,21 +1,42 @@
-#include "aInterpCurveSplineTK.h"
+#include <dip/aInterpCurveSplineTK.h>
 #include <cstdio>
+#include <algorithm>
 
 
 /**
-* Cubic TK::spline interpolation of curve.
+* Linear interpolation of curve.
 * \param f, const std::vector<aPoint<T> > & - input known (sorted by x-coordinate) curve points.
 * \param g, std::vector<aPoint<T> > & - output (sorted by x-coordinate) interpolated curve points.
 */
-template<class T>
+template<class T> 
 void aInterpCurveSplineTK<T>::make(const std::vector<aPoint<T> > &f, std::vector<aPoint<T> > &g) {
-	
+
 	std::cout << "Todo tk::spline make..." << std::endl;
 	m_isOK = true;
 	g.clear();
 
-	tk::spline s;
+	initSpline(f);
 
+	if (!m_isOK) {
+		return;
+	}
+
+	T a, b;
+
+	for ( T currentX=f[0].x; currentX<=f.back().x; currentX++ ) {
+		a = (T)currentX;
+		b = (T)m_spline_tk(currentX);
+		aPoint<T> interpolatedPoint(a, b);
+		g.push_back(interpolatedPoint);
+	}
+}
+
+/**
+* Initialize spline.
+* \param f, const std::vector<aPoint<T> > & - input known (sorted by x-coordinate) curve points.
+*/
+template<class T>
+void aInterpCurveSplineTK<T>::initSpline(const std::vector<aPoint<T> > &f) {
 	int n = f.size();
 
 	if (n <= 2) {
@@ -34,22 +55,13 @@ void aInterpCurveSplineTK<T>::make(const std::vector<aPoint<T> > &f, std::vector
 		if (f[i].x<f[i - 1].x) {
 			m_isOK = false;
 			m_lastError = "Curve points should be sorted by X-coordinate";
-			return;
+			//return;
 		}
 		x[i] = f[i].x;
 		y[i] = f[i].y;
 	} //end for
 
-	s.set_points(x, y);
-
-	T a, b;
-
-	for ( T currentX=x[0]; currentX<=x.back(); currentX++ ) {
-		a = (T)currentX;
-		b = (T)s(currentX);
-		aPoint<T> interpolatedPoint(a, b);
-		g.push_back(interpolatedPoint);
-	}
+	m_spline_tk.set_points(x, y);
 }
 
 
@@ -66,39 +78,17 @@ void aInterpCurveSplineTK<T>::compare(const std::vector<aPoint<T> > &f, int loop
 	T a = f[0].x;
 	T b = f.back().x;
 
-	tk::spline splineTK;
+	initSpline(f);
 
-	int n = f.size();
-
-	if (n <= 2) {
-		m_isOK = false;
-		m_lastError = "More than 2 points needed.";
+	if (!m_isOK) {
 		return;
 	}
-
-	std::vector<double> x(n);
-	std::vector<double> y(n);
-
-	x[0] = f[0].x;
-	y[0] = f[0].y;
-
-	for (int i = 1; i<n; i++) {
-		if (f[i].x<f[i - 1].x) {
-			m_isOK = false;
-			m_lastError = "Curve points should be sorted by X-coordinate";
-			return;
-		}
-		x[i] = f[i].x;
-		y[i] = f[i].y;
-	} //end for
-
-	splineTK.set_points(x, y);
 
 	l2 = 0.0;
 	linf = 0.0;
 	for (int i = 0; i < loopCount; i++) {
 		T x = a + (b - a) * std::rand() / (RAND_MAX + 1.0);
-		T y1 = splineTK(x);
+		T y1 = m_spline_tk(x);
 		T y2 = 0; // TO DO: method from WP3.2 spline(x);
 		T err = fabs(y1 - y2);
 		l2 += err*err;
@@ -107,7 +97,6 @@ void aInterpCurveSplineTK<T>::compare(const std::vector<aPoint<T> > &f, int loop
 	l2 = (T)(sqrt(l2) / loopCount);
 }
 
-template class aInterpCurveLinear<float>;
-template class aInterpCurveLinear<double>;
-
+template class aInterpCurveSplineTK<float>;
+template class aInterpCurveSplineTK<double>;
 
--- aInterpCurveSplineTK.h
+++ aInterpCurveSplineTK.h
@@ -5,70 +5,75 @@
 #include <utils/spline.h>
 
 /** \class aInterpCurveSplineTK
-* \brief A TK::spline interpolation of 2D curve.
-*
+* \brief A tk::spline interpolation of 2D curve. 
+* 
 * Input coordinates of know curve points coordinates, the first derivatives are not applied.
 * Output coordinates of interpolated curve points.
 * \author ooo
 * \date 2016
 */
-template<class T> class aInterpCurveSplineTK : public aInterpCurve<T> {
+template<class T> class aInterpCurveSplineTK: public aInterpCurve<T> {
 public:
 
-	//! Default constructor. 
-	aInterpCurveSplineTK() {
-		Init();
-	};
 
-	//! Virtual destructor.
-	virtual ~aInterpCurveSplineTK() {
-	};
+  //! Default constructor. 
+   aInterpCurveSplineTK() {
+     Init();
+  };
 
-	//! Calculate the convolution of imgI and imgG. oImg = imgT*imgG
-	virtual void make(const std::vector<aPoint<T> > &f, std::vector<aPoint<T> > &g);
+  //! Virtual destructor.
+  virtual ~aInterpCurveSplineTK() {
+  };
 
-	//! Compare accuracy of two spline interpolation methods.
-    virtual void compare(const std::vector<aPoint<T> > &f, method firstMethod, method secondMethod,
-	  int loopCount, T& l2, T& linf);
+  //! Calculate the convolution of imgI and imgG. oImg = imgT*imgG
+  virtual void make(const std::vector<aPoint<T> > &f, std::vector<aPoint<T> > &g);
 
-    virtual T getValue(method method, T x);
-  
-	//!
-	virtual bool isOK() {
-		return m_isOK;
-	};
+  //! Compare accuracy of two spline interpolation methods.
+  virtual void compare(const std::vector<aPoint<T> > &f, int loopCount, T& l2, T& linf);
 
-	//!
-	virtual std::string getLastError() {
-		return m_lastError;
-	};
+  //! Initialize spline
+  virtual void initSpline(const std::vector<aPoint<T> > &f);
 
-	//! Set first derivative for the first point. 
-	virtual void setDrvFirstPoint(T val) {
-		m_drvP1 = val; // not applied for this class
-	};
+  //!
+  virtual bool isOK() {
+      return m_isOK;
+  };
 
-	//! Set first derivative for the last point. Not used in this class.
-	virtual void setDrvLastPoint(T val) {
-		m_drvP2 = val; // not applied for this class
-	};
+  //!
+  virtual std::string getLastError() {
+      return m_lastError;
+  };
 
-    //! First and second derivatives.
-    T m_drvP1, m_drvP2;
-	
+  //! Set first derivative for the first point.
+  virtual void setDrvFirstPoint(T val) {
+      m_drvP1 = val; 
+  };
+
+  //! Set first derivative for the last point.
+  virtual void setDrvLastPoint(T val) {
+      m_drvP2 = val;
+  };
+
+  //! First and second derivatives.
+  T m_drvP1, m_drvP2;
+
+
 protected:
-	//! State.
-	int m_isOK;
+  //! State.
+  int m_isOK;
 
-	//! Last message.
-	std::string m_lastError;
+  //! Last message.
+  std::string m_lastError;
 
-	void Init() { // Init method
-		m_drvP1     = (T)0.0;
-		m_drvP2     = (T)0.0;
-		m_isOK = true;
-		m_lastError = "";
-	};
+  //! Spline.
+  tk::spline m_spline_tk;
+
+ void Init( ) { // Init method
+     m_drvP1     = (T)0.0;
+     m_drvP2     = (T)0.0;
+     m_isOK      = true;
+     m_lastError = "";
+  };
 
 }; // end of aInterpCurveSplineTK class description
 
