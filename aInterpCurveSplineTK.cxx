d431b4632e3e544b31d6b44f7f05bcdbb2067bc4
pasevinapolina <rigiemp@gmail.com>
2016-08-06 00:58:59 +0300

    Update aInterpCurveSplineTK.cxx

--- aInterpCurveSplineTK.cxx
+++ aInterpCurveSplineTK.cxx
@@ -4,7 +4,7 @@
 
 
 /**
-* Linear interpolation of curve.
+* tk::spline interpolation of curve.
 * \param f, const std::vector<aPoint<T> > & - input known (sorted by x-coordinate) curve points.
 * \param g, std::vector<aPoint<T> > & - output (sorted by x-coordinate) interpolated curve points.
 */
